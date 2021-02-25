92879ccd19dc97aa4ec26bbe8f9006ac2d45113d
Puneet Raj <hoenkava@gmail.com>
2021-02-26 01:16:24 +0530

    BitCoinTicker

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -34,15 +34,20 @@ const apiKey = '094415FF-58F6-46A7-82E2-27A0E22B7DD5';
 
 class coinData {
   Future getCoinData(String selectedCurrency) async {
-    http.Response getResponse = await http.get(
-        'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apiKey=094415FF-58F6-46A7-82E2-27A0E22B7DD5');
-    if (getResponse.statusCode == 200) {
-      var decodedData = jsonDecode(getResponse.body);
-      return decodedData;
+    Map<String, String> cryptoPrices = {};
+    for (String i in cryptoList) {
+      http.Response getResponse = await http.get(
+          'https://rest.coinapi.io/v1/exchangerate/$i/$selectedCurrency?apiKey=094415FF-58F6-46A7-82E2-27A0E22B7DD5');
+      if (getResponse.statusCode == 200) {
+        var decodedData = jsonDecode(getResponse.body);
+        // print(decodedData);
+        cryptoPrices[i] = decodedData['rate'].toStringAsFixed(0);
+        print(cryptoPrices);
+      } else {
+        print(getResponse.statusCode);
+        throw 'Problem with the get request';
+      }
     }
-    else{
-      print(getResponse.statusCode);
-    }
-    
+    return cryptoPrices;
   }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -7,10 +7,12 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = 'USD';
-  Map currencyRate;
-  String outPutCurrency = '1 BTC';
-  String finalRate;
+  List<Widget> currencyWidgets = [];
+  final List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
+  String selectedCurrency = 'AUD';
+  var currencyRate;
+  // String finalRate;
+
   List<DropdownMenuItem> getDropdownItems() {
     List<DropdownMenuItem<String>> dropDownItems = [];
     for (int i = 0; i < currenciesList.length; i++) {
@@ -23,26 +25,66 @@ class _PriceScreenState extends State<PriceScreen> {
     }
     return dropDownItems;
   }
-  String bitCoinValueInUsd = '?';
+
+  void onCurrencyChange(String value) {
+    setState(
+      () {
+        selectedCurrency = value;
+        getData();
+      },
+    );
+  }
+
+  Map<String, String> finalRate = {};
   void getData() async {
-    currencyRate = await coinData().getCoinData(selectedCurrency);
-    setState(() {
-      finalRate = currencyRate['rate'].toStringAsFixed(0);
-    });
-    
-    print(finalRate);
-    // print(currencyRate['rate'].toInt());
+    try {
+      currencyRate = await coinData().getCoinData(selectedCurrency);
+      setState(() {
+        finalRate = currencyRate;
+      });
+
+      print(finalRate);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    getData();
+    setState(() {
+      getData();
+    });
   }
 
   Widget build(BuildContext context) {
     getDropdownItems();
-    
+    currencyWidgets.clear();
+    cryptoList.forEach((element) {
+      currencyWidgets.add(
+        Padding(
+          padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          child: Card(
+            elevation: 10.0,
+            color: Colors.lightBlueAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+              child: Text(
+                '1 $element = ${finalRate[element]} $selectedCurrency',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    },);
     // coinData dataofCoins = new coinData();
     // dataofCoins.getCoinData(selectedCurrency);
     // List<DropdownMenuItem<String>>chize = [];
@@ -61,59 +103,41 @@ class _PriceScreenState extends State<PriceScreen> {
         ),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $finalRate USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+          ...currencyWidgets,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  height: 150.0,
+                  alignment: Alignment.center,
+                  // padding: EdgeInsets.only(bottom: 30.0),
+                  color: Colors.lightBlue,
+                  child: DropdownButton<String>(
+                    value: selectedCurrency,
+                    items: getDropdownItems(),
+                    // items: chize,
+                    // items: [
+                    //   DropdownMenuItem(
+                    //     child: Text('USD'),
+                    //     value: 'USD',
+                    //   ),
+                    //   DropdownMenuItem(
+                    //     child: Text('EUR'),
+                    //     value: 'EUR',
+                    //   ),
+                    //   DropdownMenuItem(
+                    //     child: Text('GBP'),
+                    //     value: 'GBP',
+                    //   ),
+                    // ],
+                    onChanged: onCurrencyChange,
                   ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: selectedCurrency,
-              items: getDropdownItems(),
-              // items: chize,
-              // items: [
-              //   DropdownMenuItem(
-              //     child: Text('USD'),
-              //     value: 'USD',
-              //   ),
-              //   DropdownMenuItem(
-              //     child: Text('EUR'),
-              //     value: 'EUR',
-              //   ),
-              //   DropdownMenuItem(
-              //     child: Text('GBP'),
-              //     value: 'GBP',
-              //   ),
-              // ],
-              onChanged: (value) {
-                setState(
-                  () {
-                    selectedCurrency = value;
-                  },
-                );
-              },
+                )
+              ],
             ),
           )
         ],
